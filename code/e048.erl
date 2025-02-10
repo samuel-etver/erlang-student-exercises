ee752bf38b72eb1c9218f5f5a06e78ca14a9d8ac
Samuel Etver <samuel.etver@gmail.com>
2025-02-10 17:58:30 +0500

    append exercise 048

---
+++ code/e048.erl
@@ -0,0 +1,28 @@
+% e048
+
+-module(e048).
+-export([start_timer/0]).
+-export([testAll/0]).
+
+
+start_timer() ->
+    start_timer(0).
+
+
+start_timer(Count) ->
+    timer:apply_after(1000, fun on_timer/1, [Count]).
+
+
+on_timer(Count) when Count == 10 ->
+    ok;
+on_timer(Count)->
+    start_timer(Count + 1),
+    io:fwrite("Count: ~w~n", [Count]).
+
+
+test() ->     
+    start_timer().
+
+
+testAll() ->
+    test().
